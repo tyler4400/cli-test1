2607ef1121571c6f5ef76030e8d8afff5705af98
tyler4400 <tyler4400@foxmail.com>
2025-01-22 12:28:22 +0800

    ANSI escape code学习 https://handwiki.org/wiki/:ANSI%20escape%20code

--- bin/commander.js
+++ bin/commander.js
@@ -20,7 +20,7 @@ clone
     .description('科隆项目')
     .option('-f --force', '是否强制科隆')
     .action((source, destination, cmdObj) => {
-      console.log('clone', source, destination, cmdObj);
+      console.log('\x1B[41m%s', 'clone', source, destination, cmdObj);
     })
 
 const service = new commander.Command('service')
@@ -28,13 +28,31 @@ service
     .command('start [port]')
     // .option('-p, --port [port]', 'Port of the server')
     .description('启动服务')
-    .action((port) => {console.log('启动服务', port)})
+    .action((port) => {console.log('\x1B[41m%s', '启动服务', port)})
 service
     .command('stop')
     // .option('-p, --port [port]', 'Port of the server')
     .description('停止服务')
-    .action(() => {console.log('停止服务')})
+    .action(() => {console.log('\x1B[41m%s\x1b[0m', '停止服务')})
 // 添加子命令
 program.addCommand(service)
+
+
+
+
+// red
+console.log('\x1b[31mhello imooc\x1b[39m');
+console.log('\u001b[31mhello imooc\u001b[39m');
+
+// blue
+console.log('\u001b[42m\u001b[34mhello imooc\u001b[39m\u001b[49m');
+
+// 特殊字符
+// \n
+console.log('\u001b[31mhello\u001b[39m\n\u001b[31mimooc\u001b[39m');
+// console.log(chalk.red('hello imooc\nhello imooc2\nhello imooc3'));
+// // \u001b
+// console.log(chalk.red('\u001b[31mhi~\u001b[39mall right'));
+
 
 program.parse(process.argv)
